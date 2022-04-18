d13b97ca8539203333e2f8528c771aa82d7d624d
pasindu-osadha <pasiduosanda@gmail.com>
2022-04-18 15:59:40 +0530

    initialize handson table

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import './App.css';
+import { HotTable } from '@handsontable/react';
+import { registerAllModules } from 'handsontable/registry';
+
+registerAllModules();
+
+const hotData = [
+  ["", "Tesla", "Volvo", "Toyota", "Honda"],
+  ["2020", 10, 11, 12, 13],
+  ["2021", 20, 11, 14, 13],
+  ["2022", 30, 15, 12, 13]
+];
 
 function App() {
   return (
     <div className="App">
-
+      <div id="hot-app">
+        <HotTable
+          data={hotData}
+          colHeaders={true}
+          rowHeaders={true}
+          width="600"
+          height="300"
+        />
+      </div>
     </div>
   );
 }

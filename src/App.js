c22dbdf7067b80a5905a7013ffab6df5322b6b57
pasindu-osadha <pasiduosanda@gmail.com>
2022-04-18 17:20:17 +0530

    Add custom data to table

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import { registerAllModules } from 'handsontable/registry';
 registerAllModules();
 
 const hotData = [
-  ["", "Tesla", "Volvo", "Toyota", "Honda"],
-  ["2020", 10, 11, 12, 13],
-  ["2021", 20, 11, 14, 13],
-  ["2022", 30, 15, 12, 13]
+  ["Unit", "skill 1", "skill 2", "skill 3", "capacity 1","capacity 2","capacity 3","capacity 4","rate"],
+  ["A",0.25, 0.25, 0.5, 0.2, 0.2,0.2,0.2],
+  ["B",0.35, 0.25, 0.5, 0.2, 0.2,0.2,0.2],
+  ["C",0.25, 0.25, 0.5, 0.2, 0.2,0.2,0.2]
 ];
 
 function App() {
